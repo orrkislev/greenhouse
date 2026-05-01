666e788d5eb04cbbee6a8c923f2f932ead979128
cheshierJet <tal@chamama.org>
2026-05-01 15:58:46 +0300

    remove bottom margin from dashboards

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -6,6 +6,6 @@ export const DashboardPanelButton = tw`relative text-xs text-ghdark hover:bg-ghw
     px-3 md:px-4 py-2 cursor-pointer transition-colors duration-200 whitespace-nowrap flex-shrink-0
     ${props => props.$active ? 'bg-ghwhite text-ghdarkblue font-semibold border border-ghdark' : ''}
 `;
-export const DashboardMain = tw`flex-1 overflow-y-auto z-1 mb-16`;
+export const DashboardMain = tw`flex-1 overflow-y-auto z-1`;
 export const DashboardTitle = tw`text-xl md:text-2xl font-bold text-stone-800`;
 export const DashboardSubtitle = tw`text-xs md:text-sm text-stone-500`;
